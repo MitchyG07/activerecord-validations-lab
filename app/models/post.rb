9be204d4db3114660eb283fc0d6c0f3ebe4dfbbc
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-12-17 20:45:55 -0800

    tests pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbaity 
+
+    def clickbaity
+        array = ["Won't Believe", "Secret" , "Top[number]", "Guess"]
+        if self.title 
+            unless array.any? {|word| self.title.include? word}
+                errors.add(:title, "title is not clickbaity")
+            end 
+        end 
+    end
 end
